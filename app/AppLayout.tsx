95774311566f9a901f08cbcb7e4f61f0c8bdabbc
kishanmundha <kishan.mundha@gmail.com>
2024-12-02 23:58:40 +0900

    Register icon set

--- app/AppLayout.tsx
+++ app/AppLayout.tsx
@@ -5,8 +5,12 @@ import '@headless-adminapp/fluent/styles.css';
 import 'react-quill/dist/quill.snow.css';
 import { LayoutProvider } from '@headless-adminapp/fluent/App/LayoutProvider';
 import { App } from '@headless-adminapp/fluent/App';
+import { registerIconSet } from '@headless-adminapp/icons/register';
+import { iconSet } from '@headless-adminapp/icons-fluent';
 import { PropsWithChildren } from 'react';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+
+registerIconSet(iconSet);
 
 export default function AppLayout({ children }: PropsWithChildren) {
   const router = useRouter();
