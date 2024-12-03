b5e0a6dcbc01c165c3e1cb4948f4205dce95a829
kishanmundha <kishan.mundha@gmail.com>
2024-12-03 19:47:06 +0900

    Add data form page

--- app/config/data-service.ts
+++ app/config/data-service.ts
@@ -13,4 +13,31 @@ export const dataService: IDataService = {
       ],
     };
   },
+  retriveRecord: async (logicalName, id) => {
+    return {
+      _id: 1,
+      name: 'John Doe',
+    };
+  },
+  createRecord: async (logicalName, data) => {
+    return {
+      id: '1',
+      logicalName,
+    };
+  },
+  customAction: async (actionName, payload) => {
+    return payload;
+  },
+  deleteRecord: async (logicalName, id) => {
+    return;
+  },
+  retriveAggregate: async (query) => {
+    return [];
+  },
+  updateRecord: async (logicalName, id, data) => {
+    return {
+      id,
+      logicalName,
+    };
+  },
 } as IDataService;
